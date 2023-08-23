105eb6706f8b8ccb5717d0ef5584df4aee53a677
Santiago Martinez <santiago.mbal@gmail.com>
2023-08-23 17:41:42 +0100

    Added some dependencies

---
+++ front/src/store/session.ts
@@ -0,0 +1,17 @@
+// Purpose: State management for session data
+import { StateCreator } from "zustand";
+import { type User } from "@/whombat/user";
+
+interface SessionSlice {
+  user: User | null;
+  login: (user: User) => void;
+  logout: () => void;
+}
+
+const createSessionSlice: StateCreator<SessionSlice> = (set) => ({
+  user: null,
+  login: (user: User) => set({ user }),
+  logout: () => set({ user: null }),
+});
+
+export { createSessionSlice, type SessionSlice };
