049fcfc526392e5e08c34ec3271f303f1dbd9299
mbsantiago <santiago.mbal@gmail.com>
2024-07-02 18:43:46 +0100

    Back to original tailwind darkmode

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -6,7 +6,6 @@ const config: Config = {
     "./src/lib/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: ["class", '[data-mode="dark"]'],
   theme: {
     extend: {
       backgroundImage: {
