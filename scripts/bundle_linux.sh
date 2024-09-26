650c4ce5259f94d42e97ea42d96d43809e0d2638
mbsantiago <santiago.mbal@gmail.com>
2024-09-26 20:51:15 +0100

    Updated bundle scripts to use uv instead

--- scripts/bundle_linux.sh
+++ scripts/bundle_linux.sh
@@ -1,55 +1,19 @@
 #!/bin/bash
 # Description: Bundle whombat into an executable file with pyinstaller
 
-# Move to the backend directory
-cd back/
-
-# Make sure you have python 3.12 installed
-if [[ ! $(python --version) =~ "3.12" ]]; then
-	echo "Please install python 3.12"
-	exit 1
+# Make sure you have uv installed
+if ! command -v uv &> /dev/null; then
+    echo "Please install uv"
+    exit 1
 fi
-
-# Make sure the build directory exists
-if [ ! -d "build" ]; then
-	mkdir build
-fi
-
-if [ ! -d "build/.venv" ]; then
-	# Create a virtual environment if it doesn't exist
-	python -m venv build/.venv
-fi
-
-build/.venv/bin/pip install -U pip setuptools wheel
-
-build/.venv/bin/pip install pyinstaller
-
-# Install whombat
-build/.venv/bin/pip install .
 
 # Run pyinstaller to bundle whombat into an executable file
-build/.venv/bin/pyinstaller \
-	--hidden-import "app" \
-	--hidden-import "aiosqlite" \
-	--hidden-import "colorama" \
-	--hidden-import "logging.config" \
-	--hidden-import "passlib.handlers.bcrypt" \
-    --hidden-import "rasterio" \
-	--hidden-import "rasterio.sample" \
-    --hidden-import "rasterio._shim" \
-    --hidden-import "rasterio.control" \
-    --hidden-import "rasterio.crs" \
-    --hidden-import "rasterio.vrt" \
-    --hidden-import "rasterio._features" \
-	--add-data "src/whombat/migrations:whombat/migrations" \
-	--add-data "src/whombat/statics:whombat/statics" \
-	--add-data "src/whombat/user_guide:whombat/user_guide" \
-	--add-data "alembic.ini:." \
-    --recursive-copy-metadata "numpy" \
-	--name whombat \
-	--onefile \
-	app.py
+uvx --python 3.12 --with-editable back --isolated pyinstaller whombat.spec
 
+if [ $? -ne 0 ]; then
+    echo "Failed to bundle whombat into an executable file"
+    exit 1
+fi
 
 chmod +x dist/whombat
 
