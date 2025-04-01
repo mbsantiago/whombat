9b998249c92e65554584602d7820815df544abab
mbsantiago <santiago.mbal@gmail.com>
2025-04-01 16:17:28 +0100

    Make sure sqlite foreign_keys feature is ON to ensure foreign_key restrictions
    Added an sqlalchemy event that runs on every new connection. It checks
    if the connection is to an sqlite database and if so turns on the
    foreign_key feature.

--- back/src/whombat/api/sessions.py
+++ back/src/whombat/api/sessions.py
@@ -3,7 +3,10 @@
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
-from sqlalchemy.engine import URL
+from sqlalchemy import event
+from sqlalchemy.dialects.sqlite.aiosqlite import SQLiteDialect_aiosqlite
+from sqlalchemy.engine import URL, Engine
+from sqlalchemy.engine.interfaces import DBAPIConnection
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from whombat.system import database
@@ -15,6 +18,17 @@ __all__ = [
 
 DEFAULT_DB_URL = "sqlite+aiosqlite://"
 """Default database URL to use if none is provided."""
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection: DBAPIConnection, _):
+    dbapi = getattr(dbapi_connection, "dbapi", None)
+    if not dbapi or not hasattr(dbapi, "sqlite_version"):
+        return
+
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 
 @asynccontextmanager
