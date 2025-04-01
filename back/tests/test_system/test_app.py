84a763b1160759bddff17af47b6ced408e87829c
mbsantiago <santiago.mbal@gmail.com>
2025-04-01 19:15:51 +0100

    Add test to check that the app can be initiated

---
+++ back/tests/test_system/test_app.py
@@ -0,0 +1,23 @@
+import asyncio
+
+import uvicorn
+
+from whombat.system import create_app
+from whombat.system.settings import Settings
+
+
+async def test_can_instantiate_app(test_settings: Settings):
+    app = create_app(test_settings)
+    config = uvicorn.Config(
+        app,
+        host=test_settings.host,
+        port=test_settings.port,
+        log_level=test_settings.log_level,
+        reload=False,
+    )
+    server = uvicorn.Server(config)
+
+    async with asyncio.timeout(10):
+        await server.serve()
+
+    assert server.started
